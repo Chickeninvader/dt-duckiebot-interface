fda81f2a26250d6416ae738aca34396b13e6c9bc
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-11-08 11:59:59 -0700

    try to fix key error bug traffic light

--- launchers/default.sh
+++ launchers/default.sh
@@ -12,6 +12,13 @@ dt-launchfile-init
 # NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
+# Update ROS repository key
+sudo apt-key del F42ED6FBAB17C654
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+# Update package lists
+sudo apt-get update
+
 # this is necessary for the camera pipeline to work on the Jetson Nano
 if [ "${ROBOT_HARDWARE}" == "jetson_nano" ]; then
     export LD_PRELOAD=${LD_PRELOAD}:/usr/lib/aarch64-linux-gnu/libGLdispatch.so
